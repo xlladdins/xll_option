83b33657c057b7878a5612142575eb7a640ab1c0
Keith A. Lewis <kal@kalx.net>
2020-11-15 20:13:53 -0500

    factor out variate::normal

---
+++ xll_variate_normal.cpp
@@ -0,0 +1,26 @@
+// xll_variate_normal.cpp - Excel add-in for normal variate
+#include "xll/xll/xll.h"
+#include "../../keithalewis/fmsoption/fms_variate_normal.h"
+#include "../../keithalewis/fmsoption/fms_variate.h"
+
+using namespace fms;
+using namespace xll;
+
+AddIn xai_variate_normal(
+	Function(XLL_HANDLE, "xll_variate_normal", "VARIATE.NORMAL")
+	.Args({
+		Arg(XLL_DOUBLE, "mu", "is the mean. Default is 0."),
+		Arg(XLL_DOUBLE, "sigma", "is the standard deviation. Default is 1.")
+		})
+	.Category("Option")
+	.FunctionHelp("Return handle to normal variate.")
+	.Uncalced()
+);
+HANDLEX WINAPI xll_variate_normal(double mu, double sigma)
+{
+#pragma XLLEXPORT
+	handle<variate_base<>> m(new variate_model(variate::normal(mu, sigma)));
+
+	return m.get();
+}
+
